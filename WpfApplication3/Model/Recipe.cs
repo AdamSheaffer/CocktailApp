849e48e6a0dd7b31f13a5314215409078232bc16
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-02-21 13:18:58 -0600

    Recipe constructor checks for duplicate ingredients

--- WpfApplication3/Model/Recipe.cs
+++ WpfApplication3/Model/Recipe.cs
@@ -27,11 +27,19 @@ namespace CocktailApp.Model
             this.Instructions = instructions;
             this.Name = name;
 
+            //I want to efficiently check if the ingredients in the new recipe are already in the database. If any ingredients are already in the db, they should not be added. I want to do this by touching the database the least.
+
+            IEnumerable<Ingredient> AllIngredients = IngRepo.All();
+
             foreach (Ingredient ingredient in ingredientList)
             {
-                IngRepo.AddIngredient(ingredient);
-                RecipeIngredient recipeIngredient = new RecipeIngredient(this, ingredient);
-                RecIngRepo.Add(recipeIngredient);
+                if ( AllIngredients.Contains<Ingredient>(ingredient) )
+                {
+                    IngRepo.AddIngredient(ingredient);
+                    RecipeIngredient recipeIngredient = new RecipeIngredient(this, ingredient);
+                    RecIngRepo.Add(recipeIngredient);
+                }
+                
             }
         }
         public Recipe() { }
